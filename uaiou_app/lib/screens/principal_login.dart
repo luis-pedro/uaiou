a0545343ae1c442a855bffd722b328f1a9bc30e4
Luis Pedro Costa <luispedroccosta@gmail.com>
2026-04-24 21:58:51 -0300

    Criação das primeiras telas

---
+++ uaiou_app/lib/screens/principal_login.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PrincipalLogin extends StatelessWidget {
+  const PrincipalLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(),
+    );
+  }
+}
