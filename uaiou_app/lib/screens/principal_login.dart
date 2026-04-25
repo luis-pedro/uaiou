b15e09fb4b9f0f0366b36faa83cb6c2caa34692b
Luis Pedro Costa <luispedroccosta@gmail.com>
2026-04-25 09:24:22 -0300

    Primeiros códigos

--- uaiou_app/lib/screens/principal_login.dart
+++ uaiou_app/lib/screens/principal_login.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'UaiOu',
+      home: const PrincipalLogin(),
+    );
+  }
+}
+
 class PrincipalLogin extends StatelessWidget {
   const PrincipalLogin({super.key});
 
